0fae8c50fae4e028258b1638ec0f79d3b80b5746
Andrew Alsing <andrew32alsing@gmail.com>
2024-02-07 11:31:17 -0800

    completed all challenges

--- save_to_db.py
+++ save_to_db.py
@@ -2,11 +2,13 @@
 # Split temp variable
 
 def save_into_db(info):
-    print("saved into databse")
+    print(f"{info} saved into database")
 
+username = input('Please enter your username: ')
+save_into_db(username)
 
-user_input = input('Please enter your username: ')
-save_into_db(user_input)
-user_input = int(input('Please enter your birth year: '))
-age = 2020 - user_input
-print("You are",age, "years old.")
+birth_year_input = int(input('Please enter your birth year: '))
+current_year = 2024
+age = current_year - birth_year_input
+
+print("You are", age, "years old.")
